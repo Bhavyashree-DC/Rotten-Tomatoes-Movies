7e366380116044a85340e9f68fd0ccc6915d841d
Bhavyashree_DC <bhavyadc05@gmail.com>
2024-02-07 14:38:16 +0530

    Created Navigation Bar

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+     <div class="nav-bar">
+          <div class="logo">
+              <img src="../static/logo.png" alt="logo">
+          </div>
+          <div class="search-bar">
+            <i class="ri-search-line"></i>
+            <input type="text" placeholder="Search Movies, TV, Actore, More ...">
+          </div>
+          <div class="nav-links">
+            <ul>
+              <NuxtLink to="/">Movies</NuxtLink>
+              <NuxtLink to="/">TV Shows</NuxtLink>
+              <NuxtLink to="/">Shop</NuxtLink>
+              <NuxtLink to="/">News</NuxtLink>
+              <NuxtLink to="/">Showtime</NuxtLink>
+            </ul>
+          </div>
+
+     </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +27,38 @@ export default {
   name: 'IndexPage'
 }
 </script>
+<style>
+.logo{
+   margin: 10px 20px;
+   padding: 20px;
+}
+.nav-bar {
+   width: 100%;
+   height: 150px;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   background-color: rgb(250,50,10);
+}
+.search-bar input{
+   width:600px;
+   margin: 10px 20px;
+   padding: 14px 40px;
+   background-color: rgb(150,30,6);
+   color: white;
+   font-size: 1.3rem;
+   font-weight: 600;
+   border-radius: 50px;
+   border: 2px solid white;
+}
+.nav-links{
+   display: flex;
+   align-items: center;
+   list-style: none;
+   text-decoration: none;
+   margin: 10px 80px;
+   color: white;
+   text-transform: capitalize;
+   font-size: 2rem;
+}
+</style>
